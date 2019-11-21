7e01a0c277e14ecf505c691af5c1db83d1813e18
Andrew Szymanek <andrewszymanek@Andrews-MacBook-Pro.local>
2019-11-21 11:57:53 -0600

    migrated database and showed table data so I could make sure it worked. also added properties for Superhero object

---
+++ Superheroes/Models/Superhero.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Superheroes.Models
+{
+    public class Superhero
+    {
+        public int id { get; set; }
+        public string superName { get; set; }
+        public string alterEgoName { get; set; }
+        public string primarySuperpower { get; set; }
+        public string secondarySuperpower { get; set; }
+        public string catchphrase { get; set; }
+    }
+}
