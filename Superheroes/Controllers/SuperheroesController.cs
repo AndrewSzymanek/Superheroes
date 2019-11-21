238c89d19fe83afc62f10e312b2ef21423e6c2a7
Andrew Szymanek <andrewszymanek@Andrews-MacBook-Pro.local>
2019-11-21 13:44:35 -0600

    added implementation to add and delete methods in Superheroes controller

---
+++ Superheroes/Controllers/SuperheroesController.cs
@@ -0,0 +1,98 @@
+﻿using Superheroes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Superheroes.Controllers
+{
+    public class SuperheroesController : Controller
+    {
+        ApplicationDbContext context;
+
+        public SuperheroesController()
+        {
+            context = new ApplicationDbContext();
+        }
+        // GET: Superheroes
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Superheroes/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Superheroes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Superheroes/Create
+        [HttpPost]
+        public ActionResult Create(Superhero superhero)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                context.Superheroes.Add(superhero);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Superheroes/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Superheroes/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Superheroes/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Superheroes/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, Superhero superhero)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                context.Superheroes.Remove(superhero);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
